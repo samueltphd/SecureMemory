8a49237d60289713aef1fcd1ed071762eb5ec445
Samuel Thomas <samuel_thomas@brown.edu>
2025-03-14 17:39:51 -0400

    configs,stdlib,mem: Adding Integrity Tree to gem5
    Integrity tree protected memory added to gem5 as SimObject and as a
    component in the gem5 stdlib. Usage example demonstrated in the config.

--- src/python/gem5/components/memory/secure_ddr4.py
+++ src/python/gem5/components/memory/secure_ddr4.py
@@ -53,6 +53,7 @@ from m5.objects import (
     AddrRange,
     CounterModeEncryption,
     DirectEncryption,
+    IntegrityTree,
     MemCtrl,
     Port,
 )
@@ -102,6 +103,14 @@ class SecureDDR4(AbstractMemorySystem):
             elif secure_memory_class == MAC:
                 self.secure_memory = MAC(
                     latency=latency, counter_arity=arity, cache_mac=ch
+                )
+            elif secure_memory_class == IntegrityTree:
+                self.secure_memory = IntegrityTree(
+                    latency=latency,
+                    tree_arity=arity,
+                    cache_mac=ch,
+                    eager_fetch=ef,
+                    bonsai=bonsai,
                 )
 
             self.metadata_cache = L1DCache(size=cache_size)
@@ -182,4 +191,28 @@ def MACProtectedMemory(
         cache=cache,
         cache_size=cache_size,
         ch=cache_mac,
+    )
+
+
+def IntegrityTreeProtectedMemory(
+    size: Optional[str] = "32MB",
+    latency: Optional[int] = 53,
+    arity: Optional[int] = 64,
+    cache: Optional[bool] = True,
+    cache_size: Optional[str] = "64KiB",
+    cache_mac: Optional[bool] = False,
+    eager_fetch: Optional[bool] = True,
+    bonsai: Optional[bool] = True,
+) -> AbstractMemorySystem:
+    # arity describes counter arity (number of data blocks per counter block)
+    return SecureDDR4(
+        IntegrityTree,
+        size=size,
+        latency=latency,
+        arity=arity,
+        cache=cache,
+        cache_size=cache_size,
+        ch=cache_mac,
+        ef=eager_fetch,
+        bonsai=bonsai,
     )
